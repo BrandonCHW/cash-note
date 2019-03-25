055bcb2ec8c6ce5f82fed57ca8c4e11a83b96c22
Brandon Chan <brad_1800@hotmail.com>
2019-03-24 22:15:35 -0400

    ongoing progress (background error? androidx)

--- CashNote/app/src/main/java/com/cash_note/activities/BudgetListActivity.kt
+++ CashNote/app/src/main/java/com/cash_note/activities/BudgetListActivity.kt
@@ -9,5 +9,7 @@ class BudgetListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_budget_list)
+        //val budgetListFrag = SpendingListFragment()
+        //this.addFragmentToActivity(supportFragmentManager, budgetListFrag, R.id.BudgetFragmentContainer)
     }
 }
