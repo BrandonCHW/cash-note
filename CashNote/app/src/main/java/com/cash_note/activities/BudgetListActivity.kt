48703446e53b164d831b4723c96f39976e996c00
Brandon Chan <brad_1800@hotmail.com>
2019-03-24 20:54:41 -0400

    move activities to new folder

--- CashNote/app/src/main/java/com/cash_note/activities/BudgetListActivity.kt
+++ CashNote/app/src/main/java/com/cash_note/activities/BudgetListActivity.kt
@@ -1,12 +1,13 @@
-package com.cash_note
+package com.cash_note.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.cash_note.R
 
-class SummaryActivity : AppCompatActivity() {
+class BudgetListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_summary)
+        setContentView(R.layout.activity_budget_list)
     }
 }
--- CashNote/app/src/main/java/com/cash_note/activities/SummaryActivity.kt
+++ CashNote/app/src/main/java/com/cash_note/activities/SummaryActivity.kt
@@ -1,12 +1,13 @@
-package com.cash_note
+package com.cash_note.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.cash_note.R
 
-class BudgetListActivity : AppCompatActivity() {
+class SummaryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_budget_list)
+        setContentView(R.layout.activity_summary)
     }
 }
