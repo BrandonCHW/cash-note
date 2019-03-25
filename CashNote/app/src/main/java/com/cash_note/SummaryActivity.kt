5f235a035f4e14e57f41a6b5f69e859286c8f70a
Brandon Chan <brad_1800@hotmail.com>
2019-03-24 20:19:33 -0400

    recreate project. add empty summary activity:

---
+++ CashNote/app/src/main/java/com/cash_note/SummaryActivity.kt
@@ -0,0 +1,12 @@
+package com.cash_note
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SummaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_summary)
+    }
+}
